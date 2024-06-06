6bf75f5e7222e58cf501d7a5107a521807d491d1
peihsuan1225 <jessica861225@gmail.com>
2024-06-07 02:31:19 +0800

    20240607_0230

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () =>{
     const btnRight = document.querySelector(".listbar__btn--right");
     const searchInput = document.querySelector(".heroSection__input");
 
-    fetch('/api/mrts')
+    fetch("/api/mrts")
         .then(response =>{
             if (!response.ok){
                 throw new Error("連線回應不成功"+response.statusText);
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () =>{
             console.error("fetch有問題",error);
             listbarMRT.textContent = "載入捷運站錯誤"
         }); 
+
     btnLeft.addEventListener("click", () =>{
         listbarMRT.scrollBy({
             left: -750,
@@ -42,4 +43,53 @@ document.addEventListener("DOMContentLoaded", () =>{
             behavior: "smooth"
         });
     });
+});
+
+// 顯示景點窗格
+document.addEventListener("DOMContentLoaded", () =>{
+    const attractionArea = document.querySelector(".attractionDisplayArea");
+    
+    fetch("/api/attractions")
+        .then(response =>{
+            if(!response.ok){
+                throw new Error("連線回應不成功"+response.statusText); 
+            }
+            return response.json();
+        })
+        .then(data =>{
+            if(data.data && Array.isArray(data.data)){
+                data.data.forEach(attraction => {
+                    const attractionDiv = document.createElement("div");
+                    attractionDiv.className = "attraction";
+
+                    const attractionNameDiv = document.createElement("div");
+                    attractionNameDiv.className = "attraction__text--name";
+                    attractionNameDiv.textContent = attraction.name;
+
+                    const attractionInfoDiv = document.createElement("div");
+                    attractionInfoDiv.className = "attraction__text--info";
+                    
+                    const attractionMrtDiv = document.createElement("div");
+                    attractionMrtDiv.className ="attraction__text--mrt";
+                    attractionMrtDiv.textContent = attraction.mrt;
+
+                    const attractionCatDiv = document.createElement("div");
+                    attractionCatDiv.className = "attraction__text--cat";
+                    attractionCatDiv.textContent = attraction.category;
+
+                    attractionDiv.appendChild(attractionNameDiv);
+                    attractionDiv.appendChild(attractionInfoDiv);
+                    attractionInfoDiv.appendChild(attractionMrtDiv);
+                    attractionInfoDiv.appendChild(attractionCatDiv);
+
+                    if(attraction.images && attraction.images.length >0){
+                        attractionDiv.style.backgroundImage = `url(${attraction.images[0]})`;
+                    }
+                    attractionArea.appendChild(attractionDiv);
+                });       
+            }
+        })
+        .catch(error => {
+            console.error("獲取景點數據時出錯:", error);
+        });    
 });
